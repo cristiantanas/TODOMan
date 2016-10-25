a6cf9afce085d6fa8a90c6400576a90218b16a1a
Ernesto Blanco <mail@ernestoblanco.mx>
2016-10-25 19:50:24 +0200

    Primer commit

--- todoman/src/main/java/org/uab/dedam/todoman/HomeActivity.java
+++ todoman/src/main/java/org/uab/dedam/todoman/HomeActivity.java
@@ -1,7 +1,11 @@
 package org.uab.dedam.todoman;
 
+import android.content.Intent;
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button btn_taskNew = (Button) findViewById(R.id.btn_taskNew);
+        btn_taskNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                        Intent intent=
+                                new Intent(HomeActivity.this, ContentActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
+
