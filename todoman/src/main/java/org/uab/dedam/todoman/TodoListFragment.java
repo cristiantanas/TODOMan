f34ae7f136344c12fced40bdbd3dbc470b2d3e83
bujlaki <balazs.ujlaki@gmail.com>
2016-10-26 16:37:03 +0200

    built new todo fragment layout

---
+++ todoman/src/main/java/org/uab/dedam/todoman/TodoListFragment.java
@@ -0,0 +1,29 @@
+package org.uab.dedam.todoman;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TodoListFragment extends Fragment {
+
+
+    public TodoListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_todo_list, container, false);
+    }
+
+}
